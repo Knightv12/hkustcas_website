12cfd1100ebecc3a34ca0ff04bc6be74148a1977
Knightv12 <stephenshum2001@yahoo.com.hk>
2024-08-05 22:17:11 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/hkustcas_website/",
   plugins: [react()], server:{
     host:true
   }
